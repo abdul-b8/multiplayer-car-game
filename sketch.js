b51d25fec9d99bb5d7704d7b5ff85345b30fc24e
abdul-b8 <kingabdul08@yahoo.com>
2020-12-21 17:38:33 -0500

    add file

--- sketch.js
+++ sketch.js
@@ -1,29 +1,41 @@
-var ball;
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+var cars, car1,car2, car3,car4;
+var car1_img,car2_img,car3_img,car4_img,track;
+
+function preload(){
+  car1_img=loadImage("images/car1.png");
+  car2_img=loadImage("images/car2.png");
+  car3_img=loadImage("images/car3.png");
+  car4_img=loadImage("images/car4.png");
+  track= loadImage("images/track.png");
+}
 
 function setup(){
-    createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+  canvas = createCanvas(displayWidth-20,displayHeight-20);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
 }
+
 
 function draw(){
-    background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
-    }
-    drawSprites();
-}
-
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState===2){
+    game.end();
+  }
 }
